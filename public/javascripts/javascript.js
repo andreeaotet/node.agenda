17ebd8ed6c7f97c8b5e5f6cad9e45b399716d37c
Cosmin <cosminotet@gmail.com>
2019-01-21 20:18:15 +0200

    create new contact

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,8 +1,36 @@
-function addContacts() {
-    $.ajax('data/contacts.json').done(function(contacts){
+function getNewRow() {
+    return `<tr>
+                <td><input type="text" name="firstName" placeholder="First Name"/></td>
+                <td><input type="text" name="lastName" placeholder="Last Name"/></td>
+                <td><input type="text" name="phone" placeholder="Phone"/></td>
+                <td><button onclick="saveContact()">Save</button></td>
+                </tr>`;
+}
+
+function saveContact() {
+    var firstName = $('input[name=firstName]').val();
+    var lastName = document.querySelector('input[name=lastName]').value;
+    var phone = $('input[name=phone]').val();
+
+    console.debug('saveContact...');
+
+    $.post('contacts/create', {
+        firstName,  //shortcut from ES6
+        lastName,
+        phone: phone // ES5 (key = value)
+    }).done(function(response) {
+        console.warn('done create contact', response);
+        if(response.success) {
+            loadContacts();
+        }
+    });
+}
+
+function loadContacts() {
+    $.ajax('data/contacts.json').done(function (contacts) {
         var resultList = document.querySelector('tbody');
 
-        var listItems = contacts.map(function(contact){
+        var listItems = contacts.map(function (contact) {
             console.log('transorm contact', contact);
             return `<tr>
                         <td>${contact.firstName}</td>
@@ -12,9 +40,13 @@ function addContacts() {
                     </tr>`;
         });
         console.log('list', listItems);
+        // listItems.push(getNewRow());
+
+        var actions = getNewRow()
+        listItems.push(actions);
 
         resultList.innerHTML = listItems.join('');
     });
 }
 
-addContacts();
+loadContacts();
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+// /contacts/delete?phone=1234
 router.get('/delete', function (req, res, next) {
   var phone = req.query.phone;
 
-var fs = require('fs');
-var content = fs.readFileSync('public/data/contacts.json');
-var contacts = JSON.parse(content);
+  var content = fs.readFileSync('public/data/contacts.json');
+  var contacts = JSON.parse(content);
 
-var remainingContacts = contacts.filter(function(contact) {
-  return contact.phone != phone;
-});
+  var remainingContacts = contacts.filter(function (contact) {
+    return contact.phone != phone;
+  });
 
-content = JSON.stringify(remainingContacts, null, 2);
-  fs.writeFileSync('public/data/contacts.json', remainingContacts);
+  content = JSON.stringify(remainingContacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
 
   // res.send('removing contact: ' + contacts[0].firstName);
-  res.json(remainingContacts);
+  // res.json(remainingContacts);
+  // res.json({succes: true});
 
-// TO DO please redirect to agenda.html
+  res.redirect('/agenda.html')
 
+  // TO DO please redirect to agenda.html
+
+});
+
+// /contacts/create
+router.post('/create', function (req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var phone = req.body.phone;
+
+  var content = fs.readFileSync('public/data/contacts.json');
+  var contacts = JSON.parse(content);
+
+  contacts.push({
+    firstName,
+    lastName,
+    phone
+  });
+
+  content = JSON.stringify(contacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
+
+  res.json({success: true});
 });
 
 module.exports = router;
