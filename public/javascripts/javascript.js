7c750409e688d3a183b2cecab71985b81ad253e0
Cosmin <cosminotet@gmail.com>
2019-01-28 21:07:50 +0200

    search by all fields

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,6 +1,5 @@
 var phoneToEdit = "";
 
-
 function getNewRow() {
     return `<tr>
                 <td><input type="text" name="firstName" placeholder="First Name"/></td>
@@ -15,7 +14,7 @@ function saveContact() {
     var lastName = document.querySelector('input[name=lastName]').value;
     var phone = $('input[name=phone]').val();
 
-    console.debug('saveContact...');
+    // console.debug('saveContact...');
 
     var actionUrl = phoneToEdit ? 'contacts/update?phone=' + phoneToEdit : 'contacts/create';
     
@@ -24,36 +23,37 @@ function saveContact() {
         lastName,
         phone: phone // ES5 (key = value)
     }).done(function (response) {
-        console.warn('done create contact', response);
+        // console.warn('done create contact', response);
         if (response.success) {
             loadContacts();
         }
     });
 }
 
+function displayContacts(contacts) {
+    var listItems = contacts.map(function (contact) {
+        return `<tr>
+                    <td>${contact.firstName}</td>
+                    <td>${contact.lastName}</td>
+                    <td>${contact.phone}</td>
+                    <td>
+                        <a href="/contacts/delete?phone=${contact.phone}">&#10006;</a>
+                        <a href="#" class="edit" data-id="${contact.phone}">&#9998;</a>
+                    </td>
+                </tr>`;
+    });
+    // listItems.push(getNewRow());
+    var actions = getNewRow()
+    listItems.push(actions);
+
+    var resultList = document.querySelector('tbody');
+    resultList.innerHTML = listItems.join('');
+}
+
 function loadContacts() {
     $.ajax('data/contacts.json').done(function (contacts) {
-        var resultList = document.querySelector('tbody');
         window.globalContacts = contacts;
-        var listItems = contacts.map(function (contact) {
-            console.log('transorm contact', contact);
-            return `<tr>
-                        <td>${contact.firstName}</td>
-                        <td>${contact.lastName}</td>
-                        <td>${contact.phone}</td>
-                        <td>
-                            <a href="/contacts/delete?phone=${contact.phone}">&#10006;</a>
-                            <a href="#" class="edit" data-id="${contact.phone}">&#9998;</a>
-                        </td>
-                    </tr>`;
-        });
-        console.log('list', listItems);
-        // listItems.push(getNewRow());
-
-        var actions = getNewRow()
-        listItems.push(actions);
-
-        resultList.innerHTML = listItems.join('');
+        displayContacts(contacts);
     });
 }
 
@@ -65,7 +65,7 @@ function initEvents() {
         var contact = globalContacts.find(function (contact) {
             return contact.phone == phoneToEdit;
         });
-        console.warn("edit", phoneToEdit);
+        // console.warn("edit", phoneToEdit);
 
         $('input[name=phone]').val(phoneToEdit);
         $('input[name=firstName]').val(contact.firstName);
@@ -76,16 +76,17 @@ function initEvents() {
     findContact.addEventListener('input', searchContacts);
 }
 
-var button = document.getElementById('button1');
-
 function searchContacts() {
-        // var searchFirstName = document.querySelector('input[name=firstName]').value;
-        // console.info(searchFirstName);
-        var value = this.value;
-        console.warn('please..search', this);
+        var value = this.value.toLowerCase();
+
+        var filteredContacts = globalContacts.filter(function(contact) {
+            return contact.firstName.toLowerCase().includes(value) ||
+             contact.lastName.toLowerCase().includes(value) ||
+             contact.phone.toLowerCase().includes(value)
+        });
+
+        displayContacts(filteredContacts);
 }
-
-
 
 // start app
 loadContacts();
